549793f94e4f6175aa998852dfb34ab4fb00f795
Anh Nguyen <anhnguyen150301@gmail.com>
2023-06-20 23:05:54 +0700

    fontawesome & using library for fontawesome

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
+import '~/fontawesome.js';
 import styles from './Header.module.scss';
 import images from '~/assets/image';
 
@@ -14,8 +17,16 @@ function Header() {
                 <div className={cx('logo')}>
                     <img src={images.logo} alt="logo" />
                 </div>
-                {/* {Search} */}
-                {/* {Menu} */}
+                <div className={cx('search')}>
+                    <input type="text" placeholder="Search" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                        <FontAwesomeIcon icon={['fas', 'circle-xmark']} />
+                    </button>
+                    <button>{/* Loading */}</button>
+                    <button className={cx('search')}>{/* Search */}</button>
+                </div>
+                <div classname={cx('action')}></div>
             </div>
         </header>
     );
---
+++ src/fontawesome.js
@@ -0,0 +1,6 @@
+//Khởi tạo thư viện icon của riêng bạn
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCircleXmark);
