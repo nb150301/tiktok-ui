39c5b85a00c7ce7f264303c6f55931871230d5ed
Anh Nguyen <anhnguyen150301@gmail.com>
2023-06-20 20:44:04 +0700

    steal logo svg

--- src/components/Layouts/DefaultLayout/index.js
+++ src/components/Layouts/DefaultLayout/index.js
@@ -1,13 +1,19 @@
+// components
 import Header from '~/components/Layouts/components/Header';
 import Sidebar from '~/components/Layouts/components/Sidebar';
+// styles
+import styles from './DefaultLayout.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                {children}
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
